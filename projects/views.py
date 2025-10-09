62dbb917207f4c27f83fd6f7086588d9a50d321b
zahaj <zahajkiewicz@gmail.com>
2025-10-09 20:29:34 +0200

    Feat: Create project index page with view and templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def project_index(request):
+    # This query retrieves all Project objects from the database
+    projects = Project.objects.all()
+    
+    # The context dictionary is used to pass data to the template
+    context = {
+        'projects': projects
+    }
+
+    # render() combines the template with the context data and returns an HttpResponse
+    return render(request, 'projects/project_index.html', context)
