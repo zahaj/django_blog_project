014bd3552123cdd584cd7f4318c903ef7fc03943
zahaj <zahajkiewicz@gmail.com>
2025-10-09 12:18:27 +0200

    Feat: Add image and link fields to Project model

--- projects/models.py
+++ projects/models.py
@@ -12,6 +12,8 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technologies = models.ManyToManyField(Technology)
+    image = models.ImageField(upload_to='project_images/', null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
