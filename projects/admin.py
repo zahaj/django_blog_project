61aeed432d74bc3755bc54d71eee85c77cda0770
zahaj <zahajkiewicz@gmail.com>
2025-10-09 10:40:49 +0200

    Feat: Implement ManyToMany relationship between Project and Technology

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Project, Technology # Import the Project model
+from .models import Project, Technology
+
+class ProjectAdmin(admin.ModelAdmin):
+    # This adds a nice, filterable selection box for technologies
+    filter_horizontal = ('technologies',)
 
 # Register your models here.
-admin.site.register(Project)
+# Unregister the old simple registration if it exists, and re-register with the admin class
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Technology)
--- projects/models.py
+++ projects/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
 class Technology(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.ManyToManyField(Technology)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
