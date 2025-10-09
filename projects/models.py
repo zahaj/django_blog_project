a2a89c9a23ca6a4a21868c2fee6ec19914855409
zahaj <zahajkiewicz@gmail.com>
2025-10-09 09:37:16 +0200

    Initial commit: Set up Django project with projects app and models

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
