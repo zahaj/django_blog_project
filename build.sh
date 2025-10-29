48c1405d41167fee19d96ac7a903136a2f72f28f
zahaj <zahajkiewicz@gmail.com>
2025-10-29 13:48:19 +0100

    Build: Create superuser non-interactively in build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py migrate
-python manage.py collectstatic  --noinput
+python manage.py collectstatic --noinput
 
+echo "Creating superuser..."
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists() or User.objects.create_superuser(username=os.environ['DJANGO_SUPERUSER_USERNAME'], email=os.environ['DJANGO_SUPERUSER_EMAIL'], password=os.environ['DJANGO_SUPERUSER_PASSWORD'])"
