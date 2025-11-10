79c2e4f866721051641f510a7b35e6157bf14833
zahaj <zahajkiewicz@gmail.com>
2025-11-10 16:08:05 +0100

    Refactor: Standardize URLs to use plural form (e.g., projects/)

--- projects/urls.py
+++ projects/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
     path('technology/<str:name>/', views.technology_detail, name='technology_detail'),
     path('contact', views.contact, name='contact'),
-    path('project/add', views.ProjectCreateView.as_view(), name='project_add'),
-    path('project/<int:pk>/edit', views.ProjectUpdateView.as_view(), name="project_edit"),
-    path('project/<int:pk>/delete', views.ProjectDeleteView.as_view(), name="project_delete"),
+    path('projects/add', views.ProjectCreateView.as_view(), name='project_add'),
+    path('projects/<int:pk>/edit', views.ProjectUpdateView.as_view(), name="project_edit"),
+    path('projects/<int:pk>/delete', views.ProjectDeleteView.as_view(), name="project_delete"),
 ]
